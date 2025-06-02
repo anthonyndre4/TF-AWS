2baa43961c7b296aab596d5ac2b5cc7dd856e875
Anthony Vlahos <vlahosanthony@yahoo.com>
2025-06-02 20:47:11 +0100

    fix: script file

--- ec2.tf
+++ ec2.tf
@@ -35,27 +35,8 @@ resource "aws_instance" "discord_bot" {
   vpc_security_group_ids = [aws_security_group.discord_bot_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.discord_bot_profile.name
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y docker.io awscli
-              systemctl start docker
-              systemctl enable docker
+  user_data = templatefile("${path.module}/discord-bot-userdata.sh.tmpl", {})
 
-              REGION="eu-west-2"
-              ACCOUNT_ID="682033465466"
-
-              declare -A BOTS
-              BOTS[chef_kawasaki_bot]="latest"
-              aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
-
-              for BOT in "${!BOTS[@]}"; do
-                TAG="${BOTS[$BOT]}"
-                IMAGE="$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$BOT:$TAG"
-                docker pull $IMAGE
-                docker run -d --restart always --name $BOT $IMAGE
-              done
-              EOF
 
   tags = {
     Name = "discord-bot"

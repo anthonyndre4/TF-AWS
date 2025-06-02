c927ba4c90700a9fdfc7629588a905f738c7ec2d
Anthony Vlahos <vlahosanthony@yahoo.com>
2025-06-02 21:36:57 +0100

    feat: add ecr

--- ecr.tf
+++ ecr.tf
@@ -1,9 +1,9 @@
-# locals {
-#   bots_config = yamldecode(file("${path.module}/bots.yaml"))
-#   bot_names   = local.bots_config.bots
-# }
+locals {
+  bots_config = yamldecode(file("${path.module}/bots.yaml"))
+  bot_names   = local.bots_config.bots
+}
 
-# resource "aws_ecr_repository" "bot" {
-#   for_each = toset(local.bot_names)
-#   name     = each.key
-# }
+resource "aws_ecr_repository" "bot" {
+  for_each = toset(local.bot_names)
+  name     = each.key
+}
